bf8f3aea803b47ab0c2fb50d83e35e543a903611
LEEHYUNDONG <l.steve10@yahoo.com>
2025-06-15 15:59:04 +0900

    Initial commit

---
+++ src/test/java/com/demo/book/springtdd/SpringTddBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.book.springtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

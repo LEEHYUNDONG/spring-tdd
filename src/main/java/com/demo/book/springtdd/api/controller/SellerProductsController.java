1e84e40d23a6687aa843d7390cab0d8548483fd4
LEEHYUNDONG <l.steve10@yahoo.com>
2025-07-23 16:12:14 +0900

    명령 검증 싶패 예외

--- src/main/java/com/demo/book/springtdd/api/controller/SellerProductsController.java
+++ src/main/java/com/demo/book/springtdd/api/controller/SellerProductsController.java
@@ -1,12 +1,9 @@
 package com.demo.book.springtdd.api.controller;
 
 import com.demo.book.springtdd.command.RegisterProductCommand;
-import com.demo.book.springtdd.commandmodel.InvalidCommandException;
-import com.demo.book.springtdd.domain.Product;
+import com.demo.book.springtdd.commandmodel.RegisterProductCommandExecutor;
 import com.demo.book.springtdd.domain.ProductsRepository;
-import com.demo.book.springtdd.domain.Seller;
 import com.demo.book.springtdd.domain.SellerRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,44 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.security.Principal;
-import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
-
-import static java.time.ZoneOffset.UTC;
 
 @RestController
 public record SellerProductsController(SellerRepository sellerRepository, ProductsRepository productsRepository) {
 
     @PostMapping("/seller/products")
     ResponseEntity<?> addProduct(@RequestBody RegisterProductCommand command, Principal principal) {
-        UUID sellerId = UUID.fromString(principal.getName());
-        Optional<Seller> seller = sellerRepository.findById(sellerId);
-        if(isValidUri(command.imageUri()) == false) {
-            throw new InvalidCommandException();
-        }
         UUID id = UUID.randomUUID();
-        var product = new Product();
-        product.setId(id);
-        product.setName(command.name());
-        product.setSellerId(sellerId);
-        product.setImageUri(command.imageUri());
-        product.setDescription(command.description());
-        product.setPriceAmount(command.priceAmount());
-        product.setStockQuantity(command.stockQunatity());
-        product.setRegisteredAt(LocalDateTime.now(UTC));
-        productsRepository.save(product);
-
-        URI location = URI.create("/seller/products/" + product.getId());
+        var executor = new RegisterProductCommandExecutor(productsRepository::save);
+        executor.execute(id, UUID.fromString(principal.getName()), command);
+        URI location = URI.create("/seller/products/" + id);
         return ResponseEntity.created(location).build();
-    }
-
-    private static boolean isValidUri(String imageUri) {
-        try{
-            URI uri = URI.create(imageUri);
-            return uri.getHost() != null;
-        } catch (IllegalArgumentException e) {
-            return false;
-        }
     }
 }
---
+++ src/main/java/com/demo/book/springtdd/commandmodel/RegisterProductCommandExecutor.java
@@ -0,0 +1,65 @@
+package com.demo.book.springtdd.commandmodel;
+
+import com.demo.book.springtdd.command.RegisterProductCommand;
+import com.demo.book.springtdd.domain.Product;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static java.time.ZoneOffset.UTC;
+
+public class RegisterProductCommandExecutor {
+
+    private final Consumer<Product> saveProduct;
+
+    public RegisterProductCommandExecutor(Consumer<Product> saveProduct) {
+        this.saveProduct = saveProduct;
+    }
+
+    public void execute(
+            UUID productId,
+            UUID sellerId,
+            RegisterProductCommand command
+    ) {
+        validateCommand(command);
+        Product product = createProduct(productId, sellerId, command);
+        saveProduct(product);
+    }
+
+
+    private static Product createProduct(UUID productId, UUID sellerId, RegisterProductCommand command) {
+        var product = new Product();
+        product.setId(productId);
+        product.setName(command.name());
+        product.setSellerId(sellerId);
+        product.setImageUri(command.imageUri());
+        product.setDescription(command.description());
+        product.setPriceAmount(command.priceAmount());
+        product.setStockQuantity(command.stockQunatity());
+        product.setRegisteredAt(LocalDateTime.now(UTC));
+        return product;
+    }
+
+    private void saveProduct(Product product) {
+        saveProduct.accept(product);
+    }
+
+    private static void validateCommand(RegisterProductCommand command) {
+        if(isValidUri(command.imageUri()) == false) {
+            throw new InvalidCommandException();
+        }
+    }
+
+    public static boolean isValidUri(String imageUri) {
+        try {
+            URI uri = URI.create(imageUri);
+            return uri.getHost() != null;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+
+}
